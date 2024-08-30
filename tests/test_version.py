61acb3d828ef31af532ecef94ecbcd2b326fa5a0
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-31 05:44:06 +0600

    124 intelligently create tests without sourcetest file paths (#39)
    * Update CoverageAi.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update CoverageProcessor.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update UnitTestGenerator.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update main.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update test_CoverageAi.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update test_CoverageProcessor.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update test_version.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update increase_coverage.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update main.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * Update test_version.py
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,11 +6,9 @@ from unittest.mock import mock_open, patch
 from coverage_ai.version import get_version
 
 # File location of version file is one directory up from this file's location. Use os.path to find this
-
 VERSION_FILE_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "coverage_ai/version.txt"
 )
-
 
 class TestGetVersion:
     @patch("builtins.open", new_callable=mock_open, read_data="1.2.3")
@@ -25,3 +23,10 @@ class TestGetVersion:
     @patch("builtins.open", new_callable=mock_open, read_data="   ")
     def test_get_version_empty_or_whitespace_file(self, mock_file):
         assert get_version() == ""
+
+    @patch("coverage_ai.version.sys")
+    @patch("builtins.open", new_callable=mock_open, read_data="1.2.3")
+    def test_get_version_frozen_application(self, mock_open, mock_sys):
+        mock_sys.frozen = True
+        mock_sys._MEIPASS = os.path.dirname(__file__)
+        assert get_version() == "1.2.3"
--- tests_integration/increase_coverage.py
+++ tests_integration/increase_coverage.py
@@ -8,15 +8,18 @@ from coverage_ai.CoverageAi import CoverageAi
 
 # List of source/test files to iterate over:
 SOURCE_TEST_FILE_LIST = [
-    ["coverage_ai/AICaller.py", "tests/test_AICaller.py"],
-    ["coverage_ai/CoverageProcessor.py", "tests/test_CoverageProcessor.py"],
-    ["coverage_ai/FilePreprocessor.py", "tests/test_FilePreprocessor.py"],
-    ["coverage_ai/PromptBuilder.py", "tests/test_PromptBuilder.py"],
-    ["coverage_ai/ReportGenerator.py", "tests/test_ReportGenerator.py"],
-    ["coverage_ai/Runner.py", "tests/test_Runner.py"],
-    ["coverage_ai/UnitTestGenerator.py", "tests/test_UnitTestGenerator.py"],
-    ["coverage_ai/version.py", "tests/test_version.py"],
-    ["coverage_ai/utils.py", "tests/test_load_yaml.py"],
+    # ["coverage_ai/AICaller.py", "tests/test_AICaller.py"],
+    ["coverage_ai/CoverageAi.py",               "tests/test_CoverageAi.py"],
+    # ["coverage_ai/CoverageProcessor.py", "tests/test_CoverageProcessor.py"],
+    # ["coverage_ai/FilePreprocessor.py", "tests/test_FilePreprocessor.py"],
+    # ["coverage_ai/PromptBuilder.py", "tests/test_PromptBuilder.py"],
+    # ["coverage_ai/ReportGenerator.py", "tests/test_ReportGenerator.py"],
+    # ["coverage_ai/Runner.py", "tests/test_Runner.py"],
+    # ["coverage_ai/UnitTestGenerator.py", "tests/test_UnitTestGenerator.py"],
+    # ["coverage_ai/version.py", "tests/test_version.py"],
+    # ["coverage_ai/utils.py", "tests/test_load_yaml.py"],
+    # ["coverage_ai/settings/config_loader.py", "tests/test_.py"],
+    # ["coverage_ai/CustomLogger.py",             ""],
 ]
 
 
@@ -26,14 +29,14 @@ class Args:
         self.test_file_path = test_file_path
         self.test_file_output_path = ""
         self.code_coverage_report_path = "coverage.xml"
-        self.test_command = f"poetry run pytest {test_file_path} --cov=coverage_ai --cov-report=xml --cov-report=term --log-cli-level=INFO --timeout=30"
+        self.test_command = f"poetry run pytest --cov=coverage_ai --cov-report=xml --cov-report=term --log-cli-level=INFO --timeout=30"
         self.test_command_dir = os.getcwd()
         self.included_files = None
         self.coverage_type = "cobertura"
         self.report_filepath = "test_results.html"
         self.desired_coverage = 100
         self.max_iterations = 5
-        self.additional_instructions = ""
+        self.additional_instructions = "Use as much mocking as possible"
         self.model = "gpt-4o"
         self.api_base = "http://localhost:11434"
         self.prompt_only = False
@@ -41,7 +44,7 @@ class Args:
 
 
 if __name__ == "__main__":
-    # Iterate through list of source and test files to run Cover Agent
+    # Iterate through list of source and test files to run Coverage Ai
     for source_file, test_file in SOURCE_TEST_FILE_LIST:
         # Print a banner for the current source file
         banner = f"Testing source file: {source_file}"
